d444c9e4e901127c50fa8b109699f82323a56720
Ali <anix1@kent.edu>
2023-10-31 15:09:24 -0600

    revise and refactor cypress testing files to have all passing tests

--- cypress/e2e/detailArticle_spec.cy.js
+++ cypress/e2e/detailArticle_spec.cy.js
@@ -1,7 +1,7 @@
 describe("Detail Article Page", () => {
   beforeEach(() => {
     cy.intercept(
-      "https://api.nytimes.com/svc/topstories/v2/food.json?api-key=LU6VCnan2YWTBZv4RkzC0faIiydCEDPL",
+      "https://api.nytimes.com/svc/topstories/v2/food.json?api-key=Thl0B0CyWlBvuzPKefO28SRArsuLfXJ3",
       {
         statusCode: 200,
         ok: true,
@@ -19,35 +19,33 @@ describe("Detail Article Page", () => {
       .should("eq", "http://localhost:3000/food");
   });
 
-  it.skip("Should show all article cards and be able to click on an article image and be shown more info on article", () => {
+  it("Should show all article cards and be able to click on an article image and be shown more info on article", () => {
     cy.get('[data-test="article-container"]')
       .children()
       .get(':nth-child(1) > a > [data-test="article-img"]')
-      .click();
-    // .get('[data-test="single-article-img"]')
-    // .should("be.visible")
-    // .get('[data-test="single-article-caption"]')
-    // .should("be.visible")
-    // .get('[data-test="single-article-title"]')
-    // .should("be.visible")
-    // .get('[data-test="single-article-byline"]')
-    // .should("be.visible")
-    // .get('[data-test="single-article-abstract"]')
-    // .should("be.visible");
+      .click()
+      .get('[data-test="single-article-img"]')
+      .should("be.visible")
+      .get('[data-test="single-article-title"]')
+      .should("be.visible")
+      .get('[data-test="single-article-byline"]')
+      .should("be.visible")
+      .get('[data-test="single-article-abstract"]')
+      .should("be.visible");
 
-    // cy.contains(
-    //   '[data-test="single-article-title"]',
-    //   "Mom’s Favorite Chicken Dinner Gets a Makeover"
-    // );
-    // cy.contains('[data-test="single-article-byline"]', "By Melissa Clark");
-    // cy.contains(
-    //   '[data-test="single-article-abstract"]',
-    //   "Melissa Clark takes mustardy, bread crumb-laden thighs to new heights."
-    // );
-    // cy.get('[data-test="link-details"]').contains("The New York Times").click();
+    cy.contains(
+      '[data-test="single-article-title"]',
+      "The Real End of David Chang’s Momofuku Ko Happened Years Ago"
+    );
+    cy.contains('[data-test="single-article-byline"]', "By Pete Wells");
+    cy.contains(
+      '[data-test="single-article-abstract"]',
+      "The revolutionary tasting-menu restaurant eventually came to resemble the fine-dining titans it tried to dethrone."
+    );
+    cy.get('[data-test="link-details"]').contains("The New York Times").click();
   });
 
-  it.skip("Should be able to click on the full article link", () => {
+  it("Should be able to click on the full article link", () => {
     cy.get('[data-test="article-container"]')
       .children()
       .get(':nth-child(1) > a > [data-test="article-img"]')
